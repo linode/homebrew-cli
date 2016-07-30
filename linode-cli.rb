bd994a64194d05b9d00f0b44e5d6d560d07d4add
Adam Herzog <adam@adamherzog.com>
2016-07-30 10:20:28 -0700

    Switch from sha1 to sha256.

--- linode-cli.rb
+++ linode-cli.rb
@@ -3,216 +3,216 @@ require 'formula'
 class LinodeCli < Formula
   homepage 'https://github.com/linode/cli'
   url 'https://github.com/linode/cli/archive/v1.4.7.tar.gz'
-  sha1 'b8e60becf2e90866858213e71d8040605f917794'
+  sha256 'de9ef4796028bde11776378a0ec4b50e96dd269ea2ba161a1fcdb9fb9b839b30'
 
   resource 'JSON' do
     url 'http://www.cpan.org/authors/id/M/MA/MAKAMAKA/JSON-2.90.tar.gz'
-    sha1 '8f0ffe72cbe9e6287d7ecafcf19b31cc297364c2'
+    sha256 '4ddbb3cb985a79f69a34e7c26cde1c81120d03487e87366f9a119f90f7bdfe88'
   end
 
   resource 'URI' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/URI-1.60.tar.gz'
-    sha1 '940315985af24b9f4bb5a9761b1e2bbe6c1d0df3'
+    sha256 '1f92d3dc64acb8845e9917c945e22b9a5275aeb9ff924eb7873c3b7a5c0d2377'
   end
 
   resource 'LWP::MediaTypes' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/LWP-MediaTypes-6.02.tar.gz'
-    sha1 'f56891f4e718a5f1f16f09ae37d32e454095cbed'
+    sha256 '18790b0cc5f0a51468495c3847b16738f785a2d460403595001e0b932e5db676'
   end
 
   resource 'Encode::Locale' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/Encode-Locale-1.03.tar.gz'
-    sha1 'b37c3dbb98b56d31cc8af35a548096bfbf44820f'
+    sha256 'f76337e0933225914111fcc3319ff4db359b1abfd1aa56dff2df5378db0e2d55'
   end
 
   resource 'IO::HTML' do
     url 'http://www.cpan.org/authors/id/C/CJ/CJM/IO-HTML-1.00.tar.gz'
-    sha1 '408784f567d52f26ee4ed7c10026e2c8b8e86178'
+    sha256 '04e29fd5f2535e35a9d478f2775471d79e2da6a3cf2bd33befb2dcd7811ad21a'
   end
 
   resource 'HTTP::Date' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTTP-Date-6.02.tar.gz'
-    sha1 '85f8dbcad22f2680775a185ce91a42c89e0ad2a8'
+    sha256 'e8b9941da0f9f0c9c01068401a5e81341f0e3707d1c754f8e11f42a7e629e333'
   end
 
   resource 'Compress::Raw::Bzi' do
     url 'http://www.cpan.org/authors/id/P/PM/PMQS/Compress-Raw-Bzip2-2.064.tar.gz'
-    sha1 '9c39d0ffd5414c052efa0893f03f23e59229ee8f'
+    sha256 'c3f3b9a69d06d142215b1bd7e1101f6a579a43082abba482b090389ff27b0b2b'
   end
 
   resource 'Compress::Raw::Zlib' do
     url 'http://www.cpan.org/authors/id/P/PM/PMQS/Compress-Raw-Zlib-2.065.tar.gz'
-    sha1 '09e7957e9ebedb1ba26524136adcaffe76c70cef'
+    sha256 '37b819fcd320f268d96c37ea75193f167e9e42c3025b4183543722bbdb8009c4'
   end
 
   resource 'IO::Compress' do
     url 'http://www.cpan.org/authors/id/P/PM/PMQS/IO-Compress-2.064.tar.gz'
-    sha1 '1b90096381cbabe6498985b3172b2dce99a66e94'
+    sha256 'cacfede77b7ca361865c0d08836fe8580860e046c61a39c2cd062c60be730ed9'
   end
 
   resource 'HTTP::Message' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTTP-Message-6.06.tar.gz'
-    sha1 '4a88688c28e6581fab9b2eca8d0e320e96bef1ad'
+    sha256 '087e97009c5239dca4631cf433d836771b3fc5ba5685eef1965f9d3415cbad63'
   end
 
   resource 'HTTP::Negotiate' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTTP-Negotiate-6.01.tar.gz'
-    sha1 '4a4974639d9b64f7132cb075f551f7293f788c62'
+    sha256 '1c729c1ea63100e878405cda7d66f9adfd3ed4f1d6cacaca0ee9152df728e016'
   end
 
   resource 'File::Listing' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/File-Listing-6.04.tar.gz'
-    sha1 '02a41fe1b91f3f198712965360192f31d6d8d74e'
+    sha256 '1e0050fcd6789a2179ec0db282bf1e90fb92be35d1171588bd9c47d52d959cf5'
   end
 
   resource 'HTML::Tagset' do
     url 'http://www.cpan.org/authors/id/P/PE/PETDANCE/HTML-Tagset-3.20.tar.gz'
-    sha1 'cc906acec09b23d5ffa5e1d4f40441f9de1ad777'
+    sha256 'adb17dac9e36cd011f5243881c9739417fd102fce760f8de4e9be4c7131108e2'
   end
 
   resource 'HTML::Parser' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTML-Parser-3.71.tar.gz'
-    sha1 '4f1968db53154ec54675e639000b5881962e8848'
+    sha256 'be918b3749d3ff93627f72ee4b825683332ecb4c81c67a3a8d72b0435ffbd802'
   end
 
   resource 'HTTP::Daemon' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTTP-Daemon-6.01.tar.gz'
-    sha1 'd3b7614d4b3be4b61d26011efe90026c955102a4'
+    sha256 '43fd867742701a3f9fcc7bd59838ab72c6490c0ebaf66901068ec6997514adc2'
   end
 
   resource 'Net::HTTP' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/Net-HTTP-6.06.tar.gz'
-    sha1 'd6527ec874d66c33e377a69a579ae6d66a77a5fa'
+    sha256 '1d4e3ced899efad12431564d93abe73dd2a1224aa208af5f4ab829e7a2dd39d4'
   end
 
   resource 'HTTP::Cookies' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/HTTP-Cookies-6.01.tar.gz'
-    sha1 'a8601a34e62666572bc8a4a98f56822b008afd17'
+    sha256 'f5d3ade383ce6389d80cb0d0356b643af80435bb036afd8edce335215ec5eb20'
   end
 
   resource 'WWW::RobotRules' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/WWW-RobotRules-6.02.tar.gz'
-    sha1 'e158e6559307878b32d8e4c241bf257c2bc88ebb'
+    sha256 '46b502e7a288d559429891eeb5d979461dd3ecc6a5c491ead85d165b6e03a51e'
   end
 
   resource 'libwww::perl' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/libwww-perl-6.05.tar.gz'
-    sha1 'e10b097a4a2f04efcb3549b6543e16534b46064d'
+    sha256 '7b25799ff7eec18e8e4e97dc0cad7b2a5b433b50b13feb59d9179173bee78f23'
   end
 
   resource 'Mozilla::CA' do
     url 'http://www.cpan.org/authors/id/A/AB/ABH/Mozilla-CA-20130114.tar.gz'
-    sha1 '6ea49b4e29655f629212caa3f9d5a4d3a9733e0b'
+    sha256 '82342614add1dbca8a00daa1ee55af3e0036245aed7d445537918c045008ccd7'
   end
 
   resource 'Try::Tiny' do
     url 'http://www.cpan.org/authors/id/D/DO/DOY/Try-Tiny-0.19.tar.gz'
-    sha1 '78613f8c77793032ddf10a88ef5182592b96d553'
+    sha256 '57ae3341b03af36a052922d88e1ebf1c17828b25da69dfbe98ac8b113c181aec'
   end
 
   resource 'IO::Socket::SSL' do
     url 'http://www.cpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-1.969.tar.gz'
-    sha1 '0af65f7cad1b10a30b0aa49fb66ab50fa44913c0'
+    sha256 '244361a6289615d1d9afc71225907752a345f6b5c8b82b5b5a30cc67969db2a4'
   end
 
   resource 'LWP::Protocol::https' do
     url 'http://www.cpan.org/authors/id/G/GA/GAAS/LWP-Protocol-https-6.04.tar.gz'
-    sha1 '5a63cb409ff4ba34006d5a45120e7facc52dc837'
+    sha256 '1ef67750ee363525cf729b59afde805ac4dc80eaf8d36ca01082a4d78a7af629'
   end
 
   resource 'Test::Harness' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/Test-Harness-3.30.tar.gz'
-    sha1 '5f7cc2392accaf47100cf2a57c5513c3fa29b1dc'
+    sha256 'ff1900f3b3e61321d3c4b3283298f3106d43d55446605e9cfcf1dcec036acec1'
   end
 
   resource 'Scalar::List::Utils' do
     url 'http://www.cpan.org/authors/id/P/PE/PEVANS/Scalar-List-Utils-1.44.tar.gz'
-    sha1 '2b9facc5e1145d181d546487edcc2dcf5fc91631'
+    sha256 '5bab98156f875c3e8a55667e6b11147cdcb349e65bd19dad0ff36a41e4b4fa14'
   end
 
   resource 'JSON::PP' do
     url 'http://www.cpan.org/authors/id/M/MA/MAKAMAKA/JSON-PP-2.27203.tar.gz'
-    sha1 '7659cd8b58ed42eae2c069f3181a8436c65ba895'
+    sha256 'b7063939721d0be3b0d1ab33ed13c4a8bfaecc0b526036d94d2691e582f25c52'
   end
 
   resource 'Test::Simple' do
     url 'http://www.cpan.org/authors/id/R/RJ/RJBS/Test-Simple-1.001002.tar.gz'
-    sha1 '179723fbb311d4785ede817ba7ecec43fb19d6d7'
+    sha256 '0cd3bcdb030f17a824bb35916b8200f3aa77530c6833c81b9a0e275ebfa9374c'
   end
 
   resource 'CPAN::Meta::YAML' do
     url 'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-YAML-0.018.tar.gz'
-    sha1 'e540fc207c0ace3041abfec2c90cf54d510ce9fa'
+    sha256 '33caf7c94cde58abdbd370a5ae7149635d4085c643d4838aa0ada97568821294'
   end
 
   resource 'Parse::CPAN::Meta' do
     url 'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/Parse-CPAN-Meta-1.4422.tar.gz'
-    sha1 '3f96ecfbeab892d6c309aa5d7510d3df190f50b3'
+    sha256 'd86c42d8b0a855f33f504609cee01b9f7ba4b8454a4645acd56ad66ad3f2ed33'
   end
 
   resource 'version' do
     url 'http://www.cpan.org/authors/id/J/JP/JPEACOCK/version-0.9908.tar.gz'
-    sha1 'e4514048db90dcb56a8b5a9a58ae552e8880edb7'
+    sha256 'c45647ed172443608d201461b918348f3cf2bfc7d6f72ef98c014c1d4324085b'
   end
 
   resource 'CPAN::Meta::Requirements' do
     url 'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.140.tar.gz'
-    sha1 '8dad15fa876859a175680550fe68dc09e9f3c118'
+    sha256 '0898645e8e86f0922f0f0502b503f592a8eb3d3176b4fd87adcc7ba51e751fa9'
   end
 
   resource 'CPAN::Meta' do
     url 'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-2.150005.tar.gz'
-    sha1 '2d75030bc177dedc98cc2cbd396d98943da5f86b'
+    sha256 'd218aeedf8ff9de167b8661f08f69a35ee92feb067f39b189e28eed39763e711'
   end
 
   resource 'ExtUtils::Helpers' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/ExtUtils-Helpers-0.022.tar.gz'
-    sha1 '56ebff587460bbf48b8eb93f1ad35561d5f5052d'
+    sha256 'd3f8cf700fb3414ca1260089755cbf64041455e4b744110677b1ba5bb9a3aa95'
   end
 
   resource 'ExtUtils::Config' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/ExtUtils-Config-0.007.tar.gz'
-    sha1 'dfa657a653a157d0666c2d027c3ba54edd8cf482'
+    sha256 '2c1465078b876fd16a90507092805265528c2532d4937b03547a6dbdb8ac0eef'
   end
 
   resource 'ExtUtils::InstallPaths' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/ExtUtils-InstallPaths-0.010.tar.gz'
-    sha1 '0faf32f66444ad6844aef70add175d75f2c069b2'
+    sha256 'd14cbff118e467900c2c488b55b83ef697d6eb1b8f592c0521f1d82848bf2156'
   end
 
   resource 'Module::Metadata' do
     url 'http://www.cpan.org/authors/id/E/ET/ETHER/Module-Metadata-1.000027.tar.gz'
-    sha1 'ecc9ecb20fb0950139cad433b90cc6032bd36d8e'
+    sha256 'e2f7dcb48e826d9cb4c08ca8d7e1a1d4ceaa7725f1245eb30c71ecbd119132e7'
   end
 
   resource 'Perl::OSType' do
     url 'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/Perl-OSType-1.010.tar.gz'
-    sha1 '55f74bc6b5462d6a628f396166150f683c34a039'
+    sha256 'e7ed4994b5d547cb23aadb84dc6044c5eb085d5a67a6c5624f42542edd3403b2'
   end
 
   resource 'ExtUtil' do
     url 'http://www.cpan.org/authors/id/A/AM/AMBS/ExtUtils-CBuilder-0.280224.tar.gz'
-    sha1 '5dc79083f031fcb2d1e989e0bc00526885d851f7'
+    sha256 '383131e112a3b953d699ee5e89e05580dbb08b8f62a67ff029545943d9883a6a'
   end
 
   resource 'ExtUtils::ParseXS' do
     url 'http://www.cpan.org/authors/id/S/SM/SMUELLER/ExtUtils-ParseXS-3.24.tar.gz'
-    sha1 '8c816184ea170880a54e13ea47392f35425dc8e3'
+    sha256 '30b60b8208fc9b7746ed934b678bb9618a8f28994dae8774548353a7b550371e'
   end
 
   resource 'Module::Build' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/Module-Build-0.4205.tar.gz'
-    sha1 '8b656cbc6fe813f9eec1cb63fc9d892ee8924192'
+    sha256 '1e7a597607329db642ad68ddde00c8f48d6f660ced6d2dff4c99c84e0dd1f501'
   end
 
   resource 'Module::Build::Tiny' do
     url 'http://www.cpan.org/authors/id/L/LE/LEONT/Module-Build-Tiny-0.039.tar.gz'
-    sha1 '2127b3e52d59c545ff587ca127ee1686f2a50177'
+    sha256 '7d580ff6ace0cbe555bf36b86dc8ea232581530cbeaaea09bccb57b55797f11c'
   end
 
   resource 'WebService::Linode' do
     url 'http://www.cpan.org/authors/id/M/MI/MIKEGRB/WebService-Linode-0.26.tar.gz'
-    sha1 '088e53005cccf101e11ebd1cf9360e8ec962faee'
+    sha256 '6a92511dea388b6c7fee9b3aea220c37f3bbbe49b68620a0956b4ae144f07db0'
   end
 
   def install
